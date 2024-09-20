04c4c9531a98540635708f00024d10f025355356
divyani sharma <divyani349@gmail.com>
2024-09-20 15:05:40 +0530

    Initial commit

---
+++ landing.js
@@ -0,0 +1,5 @@
+// Handle scroll animations or future interactions here
+
+window.addEventListener('scroll', function() {
+    console.log('Scroll position:', window.scrollY);
+});
---
+++ sc.js
@@ -0,0 +1,3 @@
+// This script can be extended to handle blog post data, user interactions, or animations in the future.
+
+console.log("Blog page loaded successfully!");
